15324cff51a530f8cb2f209fb7a0fc358ed159f9
Carlos Alexandre Junior <juniorpet55@hotmail.com>
2024-05-30 11:27:03 -0300

    Desafio 2 concluido!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata.Ecma335;
+
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -15,16 +17,22 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
+            // TODO Extra: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // Implementado verificação se a quantidade de hóspedes for igual a zero (0) retorna uma excessão.
+            if (hospedes.Count == 0)
+            {
+                throw new Exception ("É necessário que tenha ao menos um hóspede seja acomodado na suíte.");
+            }
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Implementado se a quantidade de hóspedes for menor ou igual a capacidade, então reserva pode ser feita.
+            else if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
-            else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // Implementado excessão ao exceder o limite de hóspedes na suíte.
+                throw new Exception ("A quantidade de hóspedes é maior que a capacidade da suíte.");
             }
         }
 
@@ -36,22 +44,22 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // Implementado o retorno da quantidade de hóspedes.
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // Implementado cálculo do valor das diárias pelos dias reservados.
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Implementado condição, caso reserva seja maior ou igual 10 dias receber desconto de 10%.
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = (DiasReservados * Suite.ValorDiaria) - ((DiasReservados * Suite.ValorDiaria) * 0.10M);
             }
 
             return valor;
